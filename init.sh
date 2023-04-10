aee3049ac88a94e2cb4f41285eaa13be4db281c3
artsiomivanets <artem.ivanec96@gmail.com>
2023-04-10 14:23:06 +0300

    remove autostart

--- init.sh
+++ init.sh
@@ -33,3 +33,4 @@ sudo pacman --needed -Suy \
 yay -S asdf-vm slack-desktop google-chrome --noconfirm
 make apply
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+sudo usermod -aG docker ${USER}
